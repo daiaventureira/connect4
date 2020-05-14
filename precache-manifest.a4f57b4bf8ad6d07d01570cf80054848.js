15fb5edc00592297484939cc550744c336d73942
Daiane Souza <dai2704@gmail.com>
2020-05-13 20:22:19 -0400

    Updates

---
+++ precache-manifest.a4f57b4bf8ad6d07d01570cf80054848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6efa2666b7ddcd31c940fbbd98d8d8d",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "f56eb0c383be813079c8",
+    "url": "/connect4/static/css/main.83d8e99c.chunk.css"
+  },
+  {
+    "revision": "b16f2ec70bbcc90c4419",
+    "url": "/connect4/static/js/2.2e01df23.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/connect4/static/js/2.2e01df23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56eb0c383be813079c8",
+    "url": "/connect4/static/js/main.09d4e3bf.chunk.js"
+  },
+  {
+    "revision": "47e988da7337e21f4924",
+    "url": "/connect4/static/js/runtime-main.7582eb53.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c5b7ec51424213b38856658b0942f991.js"
+  "/connect4/precache-manifest.a4f57b4bf8ad6d07d01570cf80054848.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/connect4/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
