af6d5e9eb49ca0ad73838957e446c0076ea7d9a4
Daiane Souza <dai2704@gmail.com>
2020-05-13 20:16:25 -0400

    Fix deploy

---
+++ build/precache-manifest.c5b7ec51424213b38856658b0942f991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa8ebb6f025af5df29900e677c3ea12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f56eb0c383be813079c8",
+    "url": "/static/css/main.83d8e99c.chunk.css"
+  },
+  {
+    "revision": "b16f2ec70bbcc90c4419",
+    "url": "/static/js/2.2e01df23.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2e01df23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56eb0c383be813079c8",
+    "url": "/static/js/main.09d4e3bf.chunk.js"
+  },
+  {
+    "revision": "48f10c271b44c5fb811f",
+    "url": "/static/js/runtime-main.6f8e8ed6.js"
+  }
+]);
